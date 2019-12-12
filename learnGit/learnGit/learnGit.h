3e4926f1c4e7a21b83f8918cdf97aa90afb585fb
yezhipeng429 <912779950@qq.com>
2019-12-12 16:08:35 +0800

    first Commit

---
+++ learnGit/learnGit/learnGit.h
@@ -0,0 +1,19 @@
+//
+//  learnGit.h
+//  learnGit
+//
+//  Created by 叶志鹏 on 2019/12/12.
+//  Copyright © 2019 叶志鹏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for learnGit.
+FOUNDATION_EXPORT double learnGitVersionNumber;
+
+//! Project version string for learnGit.
+FOUNDATION_EXPORT const unsigned char learnGitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <learnGit/PublicHeader.h>
+
+
